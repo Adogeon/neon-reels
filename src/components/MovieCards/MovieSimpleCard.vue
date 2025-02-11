65acac7c1b05da00651dc8d6742c81508d278496
Anh Dao <addao823@gmail.com>
2025-02-10 20:51:08 -0800

    Move select movie to seperate area.

--- src/components/MovieCards/MovieSimpleCard.vue
+++ src/components/MovieCards/MovieSimpleCard.vue
@@ -1,29 +1,16 @@
 <script setup lang="tsx">
 import {ref} from 'vue';
-import {RouterLink} from 'vue-router';
 import { type MovieSimple } from '../../types/Movie';
 const {data} = defineProps<{
     data: MovieSimple
 }>()
 
-const showDetail = ref<Boolean>(false);
- 
-const toggleDetail = () => {
-    showDetail.value = !showDetail.value;
-}
-
 </script>
 
 <template>
-    <div @click="toggleDetail" class="movie-card" :class="{expanded: showDetail}">
+    <div class="movie-card" >
         <img class="movie-poster" :src="`https://image.tmdb.org/t/p/w300/${data.poster_path}`"/>
         <div class="movie-title">{{ data.title }}</div>
-        <div v-if="showDetail" class="movie-details" :class="showDetail ? `show` : ``">
-            <p>{{ data.overview }}</p>
-            <p><strong>Release Date:</strong>{{ data.release_date }}</p>
-            <div><RouterLink :to="`/details/${data.id}`">Further Details ...</RouterLink></div>
-            <button @click.stop="toggleDetail"></button>
-        </div>
     </div>
 </template>
 
@@ -37,17 +24,10 @@ const toggleDetail = () => {
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   transition: transform 0.3s ease;
   cursor: pointer;
-  padding: 1rem;
-  position: relative;
 }
 
 .movie-card:hover {
   transform: translateY(-5px);
-}
-
-.movie-card.expanded {
-    height: auto;
-    z-index: 10;
 }
 
 .movie-poster {
@@ -66,16 +46,4 @@ const toggleDetail = () => {
   margin: 0.5rem;
 }
 
-.movie-details{
-  position: absolute;
-  top: 100%;
-  left: 0;
-  z-index: 5;
-  width: 100%;
-  margin-top: 0.5rem;
-  padding: 0.5rem;
-  background: themes.$card-background;
-  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3);
-  border-radius: 8px;
-}
 </style>
