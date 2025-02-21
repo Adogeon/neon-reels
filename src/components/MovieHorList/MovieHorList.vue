bbf82a92feca8af968580111414875218697fe11
Anh Dao <addao823@gmail.com>
2025-02-20 19:29:32 -0800

    Adding simple navigation to movieCard

--- src/components/MovieHorList/MovieHorList.vue
+++ src/components/MovieHorList/MovieHorList.vue
@@ -1,10 +1,14 @@
 <script setup lang="tsx">
+import { useRouter } from 'vue-router'
 import { type MovieSimple } from '../../types/Movie';
+import MovieSimpleCard from '../MovieCards/MovieSimpleCard.vue';
 
 const { title, movies } = defineProps<{
     title: string
     movies: Array<MovieSimple>
 }>()
+
+const router = useRouter();
 
 </script>
 
@@ -12,13 +16,14 @@ const { title, movies } = defineProps<{
     <section class="hor-list-container">
         <h2>{{ title }}</h2>
         <section class="movie-hor-list">
-            <MovieSimpleCard v-for="movie in movies" :data="movie" :key="movie.id" />
+            <MovieSimpleCard v-for="movie in movies" :data="movie" :key="movie.id"
+                @click="router.push({ path: `/details/${movie.id}` })" />
         </section>
     </section>
 </template>
 
 <style lang="scss" scoped>
-.hor-list.container {
+.hor-list-container {
     width: 100%;
 
     .movie-hor-list {
